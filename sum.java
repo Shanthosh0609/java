9228c84c6db2cf3da7c3879dd8b59f00a0ca58a9
Shanthosh0609 <sshanthoshp@gmail.com>
2026-06-18 11:23:05 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("number:");
+        int n=sc.nextInt();
+        while(n>10){
+            int sum=0;
+            while(n>0){
+                sum=sum+n%10;
+                n=n/10;
+            }
+            n=sum;
+        }
+        System.out.println(n);
+        sc.close();
+    }
+}
